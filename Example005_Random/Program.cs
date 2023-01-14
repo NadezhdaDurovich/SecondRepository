50e6ec404b721b20d53c9d2f88d1c768f514afdf
Nadezhda Durovich <effnylife@gmail.com>
2023-01-14 18:42:48 +0500

    Пример 005_Сумма двух случайных чисел

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,9 @@
+﻿// Работа с рандомными значениями
+int numberA=new Random().Next(1,10); // 1,2,3,4,5,6,7,8,9
+Console.WriteLine($"Первое число {numberA}");
+int numberB=new Random().Next(1,10);
+Console.WriteLine($"Второе число {numberB}");
+Console.Write("Сумма двух чисел = ");
+Console.WriteLine(numberA+numberB);
+
+
