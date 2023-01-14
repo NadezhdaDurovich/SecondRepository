6a907f4f16306e15bca98ce17933b938fcac4227
Nadezhda Durovich <effnylife@gmail.com>
2023-01-14 18:22:39 +0500

    Пример 003_Сумма двух целых чисел

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿// Пример 3_получение суммы 2-х чисел
+Console.Write("Введите первое число: ");
+int numberA=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB=Convert.ToInt32(Console.ReadLine());
+int Sum=numberA+numberB;
+Console.Write("Сумма двух чисел = ");
+Console.WriteLine(Sum);
+
+
