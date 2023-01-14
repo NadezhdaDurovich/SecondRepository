5d0f9c6b782891e7058165c7807cdc843f7fd9d3
Nadezhda Durovich <effnylife@gmail.com>
2023-01-14 18:55:13 +0500

    Пример 006_Особое приветствие для особого пользователя

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Особое приветствие для избранного пользователя
+Console.Write("Введите Ваше Имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Урра! Это же МАША!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
